32fd5a819c7a240dd5fa36feebcabfc7216eef16
jonathanengelbert <jonathanengelbert@gmail.com>
2020-05-12 14:36:32 -0400

    Fullfills Phase 2
    * Adds point data from database
    * Adds polygon data from database
    * Adds styles for layers, prototype to build new layers
    * Addresses requesting data more than once
    * Loads data from server on initial load
    * Loads data from server manually ( e.g with button )
    * Global helpers file
    * Adds map specific helpers

--- src/mapbox/mapUtils.tsx
+++ src/mapbox/mapUtils.tsx
@@ -1,11 +1,29 @@
 import mapboxgl from "mapbox-gl";
+import {Layer,} from "mapbox-gl";
 
-const getCoords = ( map: mapboxgl.Map, stateSetter: Function ) => {
-    if (!map) return ;
-    const [lng, lat ] = [map.getCenter().lng.toFixed(4), map.getCenter().lat.toFixed(4)];
-    stateSetter({currentLng: lng, currentLat: lat });
+const getCoords = (map: mapboxgl.Map, stateSetter: Function) => {
+    if (!map) return;
+    const [lng, lat] = [map.getCenter().lng.toFixed(4), map.getCenter().lat.toFixed(4)];
+    stateSetter({currentLng: lng, currentLat: lat});
 };
 
-export const mapUtils = {
-    getCoords: getCoords
+class BuildLayer implements Layer {
+    id: string;
+    source: string;
+    type: any;
+    paint: Object;
+
+    constructor(id: string, type: any, paint: Object) {
+        this.id = id;
+        this.source = id;
+        this.type = type;
+        this.paint = paint;
+    }
 }
+
+export const mapUtils = {
+    getCoords: getCoords,
+    BuildLayer: BuildLayer
+};
+
+
--- src/mapbox/mapboxStyles.tsx
+++ src/mapbox/mapboxStyles.tsx
@@ -1,9 +1,61 @@
 // TODO: darkStyle will be deprecated June 1st. Create a custom style for it
+
+// base styles
 const darkStyle = 'mapbox://styles/mapbox/dark-v10';
 const customStyle = 'mapbox://styles/jonathanengelbert/ck5egnrfz06cu1iq8x7hyy4uy';
 
-export const mapboxStyles = {
-    darkStyle: darkStyle,
-    customStyle: customStyle
+// layer styles
+class PointStyle  {
+    private readonly color: string;
+    private readonly baseRadius: number;
+
+    constructor(color?: string, baseRadius?: number) {
+        this.color = color || "white";
+        this.baseRadius = baseRadius || 1.75;
+    }
+
+    generateStyle() {
+        return {
+            // [zoomLevel, circleDiameter]
+            "circle-radius": {
+                "base": this.baseRadius,
+                "stops": [
+                    [12, 2],
+                    [22, 180]
+                ]
+            },
+            "circle-color": this.color,
+        }
+    }
 }
+
+class PolygonStyle  {
+    private readonly fillColor: string;
+    private readonly fillOutlineColor: string;
+    private readonly fillOpacity: number;
+
+    constructor(fillColor?: string, fillOpacity?: number, fillOutlineColor?: string) {
+        this.fillColor = fillColor || "white";
+        this.fillOutlineColor = fillOutlineColor || 'black';
+        this.fillOpacity = fillOpacity || 0.5;
+    }
+
+    generateStyle() {
+        return {
+            "fill-color": this.fillColor,
+            "fill-opacity": this.fillOpacity,
+            "fill-outline-color": this.fillOutlineColor,
+        }
+    }
+}
+
+export const mapboxStyles = {
+    // baseStyles
+    darkStyle: darkStyle,
+    customStyle: customStyle,
+    // layerStyles
+    pointStyle: PointStyle ,
+    polygonStyle: PolygonStyle,
+};
+
 
